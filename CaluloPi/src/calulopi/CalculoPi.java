2e861625f153b15070005cfe8f22d3c6380e8be0
Sergio Wiesner <sergio.wiesner@cirrus.net.co>
2020-03-16 12:48:08 -0500

    ya calcula pi

--- CaluloPi/src/calulopi/CalculoPi.java
+++ CaluloPi/src/calulopi/CalculoPi.java
@@ -1,49 +1,49 @@
 package calulopi;
-import java.math.BigDecimal; 
+
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class CalculoPi {
 
     public static void main(String[] args) {
         CalculoPi pi = new CalculoPi();
-        BigDecimal bd1 =  new BigDecimal("0.0"); 
-        BigDecimal resultadocalculo = pi.calularPi(bd1, 1.0, false);
-        System.out.println(resultadocalculo.toString());
-        BigDecimal resultado = resultadocalculo.subtract(new BigDecimal("1"));
-        System.out.println(resultado);
+        BigDecimal bd1 = new BigDecimal("0.0");
+        BigDecimal suma = bd1.add(new BigDecimal("1.0"));
+        BigDecimal resultadoiteraciones = pi.calularPi(suma, 1.0, false);
+        BigDecimal resultadomultiplicarpor4 = resultadoiteraciones.multiply(new BigDecimal("4"));
+        System.out.println(resultadomultiplicarpor4.toString());
     }
 
     public BigDecimal calularPi(BigDecimal data, double n, boolean signo) {
-        n = n + 2.0;
         try {
+            n = n + 2.0;
             BigDecimal operacion = new BigDecimal(String.valueOf(n));
             BigDecimal constante = new BigDecimal("1.0");
-            BigDecimal division = constante.divide(operacion,100,RoundingMode.HALF_UP);
-            System.out.println("Esta es la division: "+division.toString());
+            BigDecimal division = constante.divide(operacion, 200, RoundingMode.HALF_UP);
             if (signo) {
-                data = data.add(division); 
+                data = data.add(division);
                 signo = false;
             } else {
                 data = data.subtract(division);
                 signo = true;
             }
             data = calularPi(data, n, signo);
-        } catch (ArithmeticException e) {
-            System.out.println("Ya llego al fin se estalla por "+e.getMessage());
+        } catch (StackOverflowError error) {
+            System.out.println("Ya llega al fin de la recursion.");
+            System.out.println(error.getMessage());
         }
         return data;
     }
-    
-    
+
     public BigDecimal calularPisimple(BigDecimal data, double n, boolean signo) {
         n = n + 2.0;
-        String s=String.valueOf(n); 
+        String s = String.valueOf(n);
         BigDecimal operacion = new BigDecimal(s);
         BigDecimal constante = new BigDecimal("1.0");
         BigDecimal division = constante.divide(operacion, RoundingMode.HALF_UP);
-        data = data.add(division); 
-        System.out.println("resultado "+data.toString());
-    return data;
+        data = data.add(division);
+        System.out.println("resultado " + data.toString());
+        return data;
     }
 
 }
